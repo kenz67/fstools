b38ecfa3aa71ba1776dc3a52dcee2f00067bc1e2
Kenneth Zaldo <ken@zaldo.com>
2023-06-30 15:05:01 -0400

    Add project files.

---
+++ fstools/Models/StopwatchInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace fstools.Models;
+
+public class StopwatchInfo
+{
+    public String ElaspedTime { get; set; } = "00:00:00.0";
+    public String StartText { get; set; } = "Start";
+    public String PauseResumeText { get; set; } = "Pause";
+}
