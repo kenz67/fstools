03c96839d304013bae115183329c9538f171f1f5
Kenneth Zaldo <ken@zaldo.com>
2023-07-03 12:22:11 -0400

    Add Charts

---
+++ fstools/Models/ChartList.cs
@@ -0,0 +1,25 @@
+﻿namespace fstools.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Chart
+    {
+        public string state { get; set; }
+        public string state_full { get; set; }
+        public string city { get; set; }
+        public string volume { get; set; }
+        public string airport_name { get; set; }
+        public string military { get; set; }
+        public string faa_ident { get; set; }
+        public string icao_ident { get; set; }
+        public string chart_seq { get; set; }
+        public string chart_code { get; set; }
+        public string chart_name { get; set; }
+        public string pdf_name { get; set; }
+        public string pdf_path { get; set; }
+    }
+
+    public class ChartList
+    {
+        public List<Chart> ICAO { get; set; }
+    }
+}
