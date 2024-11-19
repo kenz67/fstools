20d94d723e9ab499b28522569eed9844ea1d45c8
Kenneth Zaldo <ken@zaldo.com>
2024-11-19 17:30:56 -0500

    Add NotesService Tests

---
+++ fstoolsTests/Services/NotesServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using fstools.Services;
+
+namespace fstoolsTests.Services
+{
+    [TestClass()]
+    public class NotesServiceTests
+    {
+        private readonly string defaultNotes = """
+        <h3 class="">
+            <u>Altitudes</u>
+        </h3>
+        <ul><li><br></li></ul>
+        <p><br></p>
+        <h3 class="">
+            <u>Frequencies</u>
+        </h3>
+        <ul><li><br></li></ul>
+        <p><br></p>
+        <h3 class="">
+            <u>Notes</u>
+        </h3>
+        <ul><li><u><br></u></li></ul>
+""";
+
+        [TestMethod()]
+        public void NotesServiceTest()
+        {
+            var svc = new NotesService();
+
+            Assert.AreEqual(defaultNotes, svc.Notes);
+        }
+
+        [TestMethod()]
+        public void SetDefaultTest()
+        {
+            var svc = new NotesService
+            {
+                Notes = "Some Notes"
+            };
+
+            Assert.AreEqual("Some Notes", svc.Notes);
+            svc.Notes = defaultNotes;
+
+            Assert.AreEqual(defaultNotes, svc.Notes);
+        }
+    }
+}
