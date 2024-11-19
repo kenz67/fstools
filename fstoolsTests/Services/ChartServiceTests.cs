2e53cd20d2940b3a2d7f9e5f10476ae7a2ab982e
Kenneth Zaldo <ken@zaldo.com>
2024-11-19 16:50:39 -0500

    ChartService Tests

---
+++ fstoolsTests/Services/ChartServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using fstools.Services;
+
+namespace fstoolsTests.Services
+{
+    [TestClass()]
+    public class ChartServiceTests
+    {
+        [TestMethod()]
+        public void ChartSvcValues()
+        {
+            var svc = new ChartService();
+
+            Assert.IsNull(svc.CurrentIcao);
+            Assert.IsNull(svc.Pdf);
+            Assert.IsFalse(svc.ShowPdf);
+
+            svc.CurrentIcao = "KFFA";
+            Assert.AreEqual("KFFA", svc.CurrentIcao);
+            Assert.IsNull(svc.Pdf);
+            Assert.IsFalse(svc.ShowPdf);
+
+            svc.Pdf = "pdf path";
+            Assert.AreEqual("KFFA", svc.CurrentIcao);
+            Assert.AreEqual("pdf path", svc.Pdf);
+            Assert.IsFalse(svc.ShowPdf);
+
+            svc.ShowPdf = true;
+            Assert.AreEqual("KFFA", svc.CurrentIcao);
+            Assert.AreEqual("pdf path", svc.Pdf);
+            Assert.IsTrue(svc.ShowPdf);
+        }
+    }
+}
