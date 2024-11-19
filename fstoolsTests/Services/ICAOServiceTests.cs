b4b9f707d353486035731817687f3f631bcd4181
Kenneth Zaldo <ken@zaldo.com>
2024-11-19 17:24:48 -0500

    Add IcaoService Tests

---
+++ fstoolsTests/Services/ICAOServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using fstools.Services;
+
+namespace fstoolsTests.Services
+{
+    [TestClass()]
+    public class IcaoServiceTests
+    {
+        [TestMethod()]
+        public void ChartSvcValues()
+        {
+            var svc = new IcaoService();
+            Assert.IsNull(svc.IcaoList);
+
+            svc.IcaoList = [];
+            Assert.AreEqual(0, svc.IcaoList.Count);
+
+            svc.IcaoList.Add("KOXC");
+            svc.IcaoList.Add("N87");
+
+            Assert.AreEqual(2, svc.IcaoList.Count);
+        }
+    }
+}
