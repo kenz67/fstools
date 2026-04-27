7eba83583c64e1b8b3f097d8ff8196a138b9fd28
Kenneth Zaldo <ken@zaldo.com>
2026-04-27 14:49:33 -0400

    Romve Scrape Tests

--- fstoolsTests/Services/SkyVectorScrapeTests.cs
+++ fstoolsTests/Services/SkyVectorScrapeTests.cs
@@ -1,23 +1,21 @@
-﻿using fstools.Services;
-using Xunit;
-
-namespace fstoolsTests.Services;
+﻿namespace fstoolsTests.Services;
 
 public class SkyVectorScrapeTests
 {
-    [Fact]
-    public async Task ScrapeTest()
-    {
-        var svc = new SkyVectorScrape();
-        var charts = await svc.Scrape("KJFK");
-        Assert.NotEmpty(charts.ICAO);
-    }
+	//TODO
+	//[Fact]
+	//public async Task ScrapeTest()
+	//{
+	//    var svc = new SkyVectorScrape();
+	//    var charts = await svc.Scrape("KJFK");
+	//    Assert.NotEmpty(charts.ICAO);
+	//}
 
-    [Fact]
-    public async Task ScrapeNegativeTest()
-    {
-        var svc = new SkyVectorScrape();
-        var charts = await svc.Scrape("KZZZ");
-        Assert.Empty(charts.ICAO);
-    }
+	//[Fact]
+	//public async Task ScrapeNegativeTest()
+	//{
+	//    var svc = new SkyVectorScrape();
+	//    var charts = await svc.Scrape("KZZZ");
+	//    Assert.Empty(charts.ICAO);
+	//}
 }
