c3f4a8ca33e75d3513f53f8bf66845a42d99db64
Kenneth Zaldo <ken@zaldo.com>
2026-05-01 10:26:46 -0400

    Update Tests

--- fstoolsTests/Services/ChartServiceTests.cs
+++ fstoolsTests/Services/ChartServiceTests.cs
@@ -5,28 +5,28 @@ namespace fstoolsTests.Services;
 
 public class ChartServiceTests
 {
-    [Fact]
-    public void ChartSvcValues()
-    {
-        var svc = new ChartService();
+	[Fact]
+	public void ChartSvcValues()
+	{
+		var svc = new ChartService();
 
-        Assert.Null(svc.CurrentIcao);
-        Assert.Null(svc.Pdf);
-        Assert.False(svc.ShowPdf);
+		Assert.Empty(svc.CurrentIcao);
+		Assert.Null(svc.Pdf);
+		Assert.False(svc.ShowPdf);
 
-        svc.CurrentIcao = "KFFA";
-        Assert.Equal("KFFA", svc.CurrentIcao);
-        Assert.Null(svc.Pdf);
-        Assert.False(svc.ShowPdf);
+		svc.CurrentIcao = "KFFA";
+		Assert.Equal("KFFA", svc.CurrentIcao);
+		Assert.Null(svc.Pdf);
+		Assert.False(svc.ShowPdf);
 
-        svc.Pdf = "pdf path";
-        Assert.Equal("KFFA", svc.CurrentIcao);
-        Assert.Equal("pdf path", svc.Pdf);
-        Assert.False(svc.ShowPdf);
+		svc.Pdf = "pdf path";
+		Assert.Equal("KFFA", svc.CurrentIcao);
+		Assert.Equal("pdf path", svc.Pdf);
+		Assert.False(svc.ShowPdf);
 
-        svc.ShowPdf = true;
-        Assert.Equal("KFFA", svc.CurrentIcao);
-        Assert.Equal("pdf path", svc.Pdf);
-        Assert.True(svc.ShowPdf);
-    }
+		svc.ShowPdf = true;
+		Assert.Equal("KFFA", svc.CurrentIcao);
+		Assert.Equal("pdf path", svc.Pdf);
+		Assert.True(svc.ShowPdf);
+	}
 }
