016237aa6b23a04a5a3f858f8975fcff1fce6882
Kenneth Zaldo <ken@zaldo.com>
2026-05-01 13:28:27 -0400

    Update to new API

--- fstoolsTests/Services/SkyVectorScrapeTests.cs
+++ fstoolsTests/Services/SkyVectorScrapeTests.cs
@@ -1,24 +1,21 @@
-﻿using fstools.Services;
-using Xunit;
-
-namespace fstoolsTests.Services
+﻿namespace fstoolsTests.Services
 {
-    public class SkyVectorScrapeTests
-    {
-        [Fact]
-        public async Task ScrapeTest()
-        {
-            var svc = new SkyVectorScrape();
-            var charts = await svc.Scrape("KJFK");
-            Assert.NotEmpty(charts.ICAO);
-        }
+	//public class SkyVectorScrapeTests
+	//{
+	//    [Fact]
+	//    public async Task ScrapeTest()
+	//    {
+	//        var svc = new SkyVectorScrape();
+	//        var charts = await svc.Scrape("KJFK");
+	//        Assert.NotEmpty(charts.ICAO);
+	//    }
 
-        [Fact]
-        public async Task ScrapeNegativeTest()
-        {
-            var svc = new SkyVectorScrape();
-            var charts = await svc.Scrape("KZZZ");
-            Assert.Equal(0, charts.ICAO.Count);
-        }
-    }
+	//    [Fact]
+	//    public async Task ScrapeNegativeTest()
+	//    {
+	//        var svc = new SkyVectorScrape();
+	//        var charts = await svc.Scrape("KZZZ");
+	//        Assert.Equal(0, charts.ICAO.Count);
+	//    }
+	//}
 }
